69139289ac46cb305b834c47e322e0c5de0148e2
hectick <ch.yeondev@gmail.com>
2023-12-09 01:51:25 +0900

    feat: 스프링 프로젝트 생성

---
+++ everydaymail/src/main/java/com/mkcy/everydaymail/EverydaymailApplication.java
@@ -0,0 +1,13 @@
+package com.mkcy.everydaymail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverydaymailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverydaymailApplication.class, args);
+	}
+
+}
---
+++ everydaymail/src/test/java/com/mkcy/everydaymail/EverydaymailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkcy.everydaymail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverydaymailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
